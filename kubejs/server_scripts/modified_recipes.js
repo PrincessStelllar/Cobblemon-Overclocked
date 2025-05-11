c237fb47f6144c5bb2a79bbb7908b112c6b40b47
jaker.zzz <modifiedminecrafters@gmail.com>
2025-05-10 22:14:08 -0400

    recipe additions - struc comp

--- kubejs/server_scripts/modified_recipes.js
+++ kubejs/server_scripts/modified_recipes.js
@@ -16,8 +16,38 @@ ServerEvents.recipes(event => {
       G: 'minecraft:gold_block',
       M: 'minecraft:emerald_block'
     })
-})
+});
 
 ServerEvents.recipes(event=> {
   event.remove({id: "cobblemon:master_ball"})
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shaped(
+  'cobblemon:tart_apple',
+  [
+    ' G ',
+    'GBG',
+    ' G '
+  ],
+  {
+    B: 'biomeswevegone:green_apple',
+    G: 'minecraft:gold_block'
+  })
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shaped(
+  'cobblemon:sweet_apple',
+  [
+    ' S ',
+    'SAS',
+    ' S '
+  ],
+  {
+    A: 'minecraft:golden_apple',
+    S: 'supplementaries:sugar_cube'
+  })
 })
